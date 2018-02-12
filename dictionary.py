c4ef7d9f1d45686175c90616b270a5613325ea52
Viusphorna <35180999+Viusphorna@users.noreply.github.com>
2018-02-12 18:35:30 +1100

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+f = open('dictin.txt', 'r')
+l = f.readline().split()
+d = int(l[0])
+w = int(l[1])
+trans = {}
+for i in range(d):
+    y = (f.readline().split())
+    t = int(y[0])
+    r = int(y[1])
+    trans[t] = r
+s = open('dictout.txt', 'w')
+for k in range(w):
+    q = (f.readline().split())
+    h = int(q[0])
+    if (h in trans):
+        s.write(str(trans[h]) + '\n')
+    else:
+        s.write('C?')
